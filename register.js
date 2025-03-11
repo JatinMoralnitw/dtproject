e38d358a4116411f6affb176cfa15d86085f4cb5
JatinMoralnitw <jm24csb0b28@student.nitw.ac.in>
2025-03-11 13:07:26 +0530

    Add files via upload

---
+++ register.js
@@ -0,0 +1,24 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+   https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBH-Krz3pEVvM3M-g5ynNMN4yYq9Ayq7aM",
+    authDomain: "yogadt-6557b.firebaseapp.com",
+    projectId: "yogadt-6557b",
+    storageBucket: "yogadt-6557b.firebasestorage.app",
+    messagingSenderId: "992559424375",
+    appId: "1:992559424375:web:f0e9d23a24b65346e3f772",
+    measurementId: "G-76BS6919CK"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
+
